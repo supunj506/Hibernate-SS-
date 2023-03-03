72374c7e3376a7b15a604f83bb3856fc4e60ae94
unknown <you@example.com>
2023-03-03 20:13:15 +0530

    create FactoryConfiguration class with single_turn

---
+++ src/lk/ccns/thogakade/util/FactoryConfiguration.java
@@ -0,0 +1,30 @@
+/*
+ * @author : Supun Jaysinghe
+ * Date    : 03 March 2023
+ * Time    : 8:07 PM
+ * Project : Thgakade With Hibernate
+ * Created by IntelliJ IDEA.
+ */
+package lk.ccns.thogakade.util;
+
+import lk.ccns.thogakade.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration configuration=new Configuration().configure().addAnnotatedClass(Customer.class);
+        sessionFactory=configuration.buildSessionFactory();
+    }
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null)?factoryConfiguration=new FactoryConfiguration():factoryConfiguration;
+    }
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
