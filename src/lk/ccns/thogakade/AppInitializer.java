7e8c45648fe0a442f8c8c030b8b2ed352cbb92e4
unknown <you@example.com>
2023-03-03 20:17:59 +0530

    launch Session Successfully

--- src/lk/ccns/thogakade/AppInitializer.java
+++ src/lk/ccns/thogakade/AppInitializer.java
@@ -11,10 +11,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lk.ccns.thogakade.entity.Customer;
+import lk.ccns.thogakade.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class AppInitializer extends Application {
     public static void main(String[] args) {
-        launch(args);
+//        launch(args);
+        Customer customer1=new Customer("C001","Kasun Sankalpa","Meegahathanna",40000);
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction= session.beginTransaction();
+        session.save(customer1);
+        transaction.commit();
+        session.close();
     }
 
     @Override
