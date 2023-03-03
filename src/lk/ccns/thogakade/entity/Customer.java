c051e4d8605bbeac308328a59472f21858fe18ec
unknown <you@example.com>
2023-03-03 20:06:26 +0530

    create customer entity class

---
+++ src/lk/ccns/thogakade/entity/Customer.java
@@ -0,0 +1,62 @@
+/*
+ * @author : Supun Jaysinghe
+ * Date    : 03 March 2023
+ * Time    : 8:04 PM
+ * Project : Thgakade With Hibernate
+ * Created by IntelliJ IDEA.
+ */
+package lk.ccns.thogakade.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, String address, double salary) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
